f102f5953741e35c53950fc42c46a78bad30b60e
PedroDev11 <pedro2076rr@gmail.com>
2023-11-29 22:45:17 -0600

    FastAPI Full-Stack

---
+++ main.py
@@ -0,0 +1,24 @@
+#uvicorn main:app --reload
+#pip install -r requirements.txt
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from starlette import status
+from starlette.responses import RedirectResponse
+
+import models
+from database import engine
+from routers import auth, todos, users
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get('/')
+async def root():
+    return RedirectResponse(url='/todos', status_code=status.HTTP_302_FOUND)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(users.router)
